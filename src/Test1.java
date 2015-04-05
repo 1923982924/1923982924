f7c88c4a7a555c5a8197f9a40f858c80c9e81724
chenlong <1923982924@qq.com>
2015-04-05 10:43:54 +0800

    first use git

---
+++ src/Test1.java
@@ -0,0 +1,37 @@
+
+public class Test1 implements C{
+
+	public static void main(String[] args){
+		new Thread(new Runnable(){  
+            public void run(){  
+            	System.out.println("start...");
+            }}).start();  
+	}
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void test1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void test2() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+interface A{
+	void test();
+} 
+interface B{
+	void test1();
+}
+interface C extends A,B{
+	void test2();
+}
